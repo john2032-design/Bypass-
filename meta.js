c24cd21f6020ed331e2d4970881738f5b4e5d5a5
john2032-design <faysal.heshmatullah@gmail.com>
2025-11-13 16:19:55 -0400

    Update meta.js

--- meta.js
+++ meta.js
@@ -1,5 +1,5 @@
 {
-  "version": "1.3",
+  "version": "1.4",
   "updateUrl": "https://afk-bypasser.vercel.app/update?version=1.3",
   "downloadUrl": "https://raw.githubusercontent.com/john2032-design/Bypass-/refs/heads/main/afk-bypasser.user.js",
   "changelog": "Added auto-update feature and improved compatibility"
