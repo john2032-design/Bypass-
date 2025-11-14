a6b09987855e5ed8e0da1bcac904338052fed891
john2032-design <faysal.heshmatullah@gmail.com>
2025-11-13 22:35:23 -0400

    Update meta.js

--- meta.js
+++ meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AFK Bypasser Helper
 // @namespace    https://afk-bypasser.vercel.app
-// @version      1.6
+// @version      1.7
 // @description  Automatically redirects supported shorteners to AFK Bypasser
 // @author       AFK Bypasser
 // @icon         https://raw.githubusercontent.com/john2032-design/Bypass-/refs/heads/main/B57FBD3E-489E-4F0D-A5C0-08017DA44C4E.png
@@ -61,6 +61,7 @@
 // @match        *://*ytsubme.com/*
 // @match        *://krnl.cat/*
 // @match        *://keyrblx.com/*
+// @match        *://pandadevelopment/*
 // @updateURL    https://afk-bypasser.vercel.app/meta.js
 // @downloadURL  https://raw.githubusercontent.com/john2032-design/Bypass-/refs/heads/main/afk-bypasser.user.js
 // @grant        none
