6ee0f550525e91ad01f2e0cc3142a5ad302555d4
John <faysal.heshmatullah@gmail.com>
2025-11-29 18:54:02 +0000

    Update afk-bypasser.user.js

--- afk-bypasser.user.js
+++ afk-bypasser.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AFK Bypasser Helper
 // @namespace    https://afk-bypasser.vercel.app
-// @version      2.1
+// @version      2.2
 // @description  Automatically redirects supported shorteners to AFK Bypasser
 // @author       AFK Bypasser
 // @icon         https://raw.githubusercontent.com/john2032-design/Bypass-/refs/heads/main/B57FBD3E-489E-4F0D-A5C0-08017DA44C4E.png
@@ -103,7 +103,7 @@
     
     const redirectWaitTime = 5;
     const bypasserSite = "https://afk-bypasser.vercel.app";
-    const currentVersion = "2.1";
+    const currentVersion = "2.2";
     
     function checkForUpdate() {
         if (window.location.href.includes('afk-bypasser.vercel.app')) return;
